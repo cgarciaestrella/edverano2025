9055bf7078055581a267a81fd4c64c6f65a104cc
cgarciaestrella <cgarcia@unsm.edu.pe>
2025-01-15 08:55:51 -0500

    Proyectos de Estructura de datos con JAVA

--- palabra/src/App.java
+++ palabra/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Letras en un arreglo");
+        String palabra = "Uwu, Oño, XD, :3";
+        int t = palabra.length();
+        String [] a = new String[t];
+        for(int i=0;i<=t-1;i++){
+            String cad = palabra.substring(i, i+1);
+            a[i] = cad;
+        }
+        for(int i=0;i<=t-1;i++){
+            System.out.print(a[i]);
+        }
+
     }
 }
