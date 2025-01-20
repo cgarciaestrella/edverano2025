25cbbd3e6539fbc265bf1f9ece8759588b4f49d0
cgarciaestrella <cgarcia@unsm.edu.pe>
2025-01-20 07:55:07 -0500

    Proyectos de Estructura de datos con JAVA

---
+++ ordenardigitos/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Ordenamiento de dígitos de un número mayor que 9");
+        int n = 625803;
+        if (n>9) {
+            int m = n;
+            int d = 0;
+            while (m>0) {
+                m = m / 10;
+                d = d + 1;
+            }            
+            int [] a = new int[d];
+            for(int i=0; i<d; i++){
+                int x = n % 10;
+                a[i] = x;
+                n = n / 10;                
+            }               
+        }
+        else{
+            System.out.println("XD");
+        }
+    }
+}
--- palabra/src/App.java
+++ palabra/src/App.java
@@ -3,14 +3,16 @@ public class App {
         System.out.println("Letras en un arreglo");
         String palabra = "Uwu, Oño, XD, :3";
         int t = palabra.length();
-        String [] a = new String[t];
+        String [] a = new String[t]; // Crear el arreglo
+        
+        // Ingresa los datos en el arreglo
         for(int i=0;i<=t-1;i++){
             String cad = palabra.substring(i, i+1);
             a[i] = cad;
         }
+        // Mostrar el arreglo
         for(int i=0;i<=t-1;i++){
             System.out.print(a[i]);
         }
-
     }
 }
