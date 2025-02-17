1ea8b3e6bfb84b126b72bcce6bd6b2964f6bc5fc
cgarciaestrella <cgarcia@unsm.edu.pe>
2025-02-17 10:14:39 -0500

    Ejercicios de JAVA desarrollados en clases

--- ejercicio8separata4/src/App.java
+++ ejercicio8separata4/src/App.java
@@ -19,12 +19,36 @@ public class App {
                 }
             }
             // Aquí se debe evaluar los corchetes y paréntesis
-            
-            System.out.println("Los elementos de la pila son: ");
+            int cc = 0; int cp = 0; String elemento;
             for (int i = cima; i >= 0; i--) {
-                System.out.println(pila[i]);
+                elemento = pila[i];
+                switch (elemento) {
+                    case "[":
+                        cc = cc + 1; break;
+                    case "]":
+                        cc = cc - 1; break;
+                    case "(":
+                        cp = cp + 1; break;
+                    case ")":
+                        cp = cp - 1; 
+                        break;
+                }
             }
-
+            if (cima == -1) {
+                System.out.println("La pila está vacía");
+            }
+            else{
+                System.out.println("Los elementos de la pila son: ");
+                for (int i = cima; i >= 0; i--) {
+                    System.out.println(pila[i]);
+                }
+                if(cc == 0 && cp == 0 ){
+                    System.out.println("La expresión está balanceada");           
+                }
+                else{
+                    System.out.println("La expresión no está balanceada");
+                }
+            }
         }
         else{
             System.out.println("No existe cadena para operar");
